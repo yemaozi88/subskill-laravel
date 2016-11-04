8c679da0e13442432a617a97db88bf9393efa2ce
乐俊伟 <316586134@qq.com>
2016-11-05 06:29:08 +0900

    add admin detail eword

--- app/Http/Controllers/Admin/EwordController.php
+++ app/Http/Controllers/Admin/EwordController.php
@@ -41,7 +41,7 @@ class EwordController extends Controller
         $date = $request->input('date');
 
         // TODO(sonicmisora): Add group constraint
-        $results = EwordResult::select('session_id', 'is_correct', 'elapsed_time')
+        $results = EwordResult::select('session_id', 'is_correct', 'elapsed_time', 'quiz_index')
             ->whereIn('session_id', function ($query) use ($date, $test_type) {
                 $query->selectRaw('id')
                     ->from('eword_sessions')
@@ -55,7 +55,9 @@ class EwordController extends Controller
                 $query->select('id', 'username');
             }])
             ->get();
-        $overall_stat = $this->compute_stat($results);
+        // TODO(sonicmisora): make 'R' configurable
+        $q_table = array_map('str_getcsv', file(public_path("upload/listening/setR/filelist.csv")));
+        $overall_stat = $this->compute_stat($results, $q_table);
 
         return view('admin/eword/detail', [
             'date' => $date,
@@ -65,7 +67,66 @@ class EwordController extends Controller
     }
 
     // Private functions
-    private function compute_stat($results) {
+    private function compute_stat($results, $q_table) {
+        // Number of questions
+        //$q_num = count($q_table);
+        // Level for each question
+        $qindex2level = [];
+        // Number of questions for each level
+        $nums_of_level = [];
+        foreach ($q_table as $value) {
+            $qindex2level[$value[0]] = $value[7];
+            if (!key_exists($value[7], $nums_of_level)) {
+                $nums_of_level[$value[7]] = 0;
+            }
+            $nums_of_level[$value[7]]++;
+        }
+        ksort($nums_of_level);
 
+        // accumulate statistics
+        $acc_stats = [];
+        foreach ($results as $entry) {
+            $username = $entry->session->user->username;
+            if (!key_exists($username, $acc_stats)) {
+                $acc_stats[$username] = [];
+            }
+            array_push($acc_stats[$username], [
+                'i' => $entry->quiz_index,
+                'c' => $entry->is_correct,
+                'e' => $entry->elapsed_time,
+            ]);
+        }
+
+        // Overal return table
+        $ret = [
+            'correct_rate' => [],
+        ];
+        // correct rate
+        foreach ($acc_stats as $username => $stat) {
+            $cr = $this->compute_stat_single($stat, 'c');
+            $ret['correct_rate'][$username] = ['t' => $cr];
+            foreach ($nums_of_level as $level => $num) {
+                $level_stat = array_filter($stat, function($v) use ($qindex2level, $level) {
+                    return $level == $qindex2level[$v['i']];
+                });
+                $cr = $this->compute_stat_single($level_stat, 'c');
+                //var_dump($level);
+                $ret['correct_rate'][$username][$level] = $cr;
+            }
+        }
+
+        return $ret;
+    }
+
+    private function compute_stat_single($stat, $column) {
+        $cnt = count($stat);
+        if ($cnt == 0) {
+            return 0.0;
+        }
+        $s = 0;
+        foreach ($stat as $row) {
+            $s += $row[$column];
+        }
+        return $s / $cnt;
     }
 }
--- resources/views/admin/eword/detail.blade.php
+++ resources/views/admin/eword/detail.blade.php
@@ -12,11 +12,32 @@
             </div>
             <div class="panel-body">
                 <h4 class="subtitle">正答率[%]</h4>
+                @if (count($overall_stat['correct_rate']) > 0)
+                <table class="table table-hover">
+                    <tr>
+                        <th>氏名</th>
+                        @foreach(reset($overall_stat['correct_rate']) as $key => $value)
+                        <th>{{ $key == 't' ? '全体' : "レベル$key" }}</th>
+                        @endforeach
+                    </tr>
+                    @foreach($overall_stat['correct_rate'] as $username => $entry)
+                        <tr>
+                            <td>{{ $username }}</td>
+                            @foreach($entry as $key => $value)
+                                <td>{{ $value * 100.0 }}</td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </table>
+                @else
+                <p>ガラ空き</p>
+                @endif
             </div>
         </div>
     </table>
 
     <div>
+      
         @foreach ($results as $result)
             {{ $result }}<br>
         @endforeach
